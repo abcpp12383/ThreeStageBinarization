a10d39c28e11cd4f99d6e503b96ca007cdfb16e6
abcpp12383 <77450155+abcpp12383@users.noreply.github.com>
2022-11-28 16:50:21 +0800

    Add files via upload

---
+++ DIBCO/Base/image_dwt_256.py
@@ -0,0 +1,190 @@
+import cv2
+from cv2 import imshow
+import numpy as np 
+import matplotlib.pyplot as plt
+import glob
+import os
+from PIL import Image
+import pywt
+
+def main():
+    image_dir = '../ThreeStageBinarization/image_patches/256X256/'
+    rename_dir = '../ThreeStageBinarization/image_patches/256X256_rename/'
+    gray_dir = '../ThreeStageBinarization/image_patches/256X256_gray/'
+    turn_img_to_gray(image_dir,rename_dir,gray_dir)
+    split_3_colors = '../ThreeStageBinarization/image_patches/256X256_split_colors/'
+    image_split_3_colors(image_dir,split_3_colors)
+    DWT_4_image_dir = '../ThreeStageBinarization/image_patches/256X256_DWT_image/'
+    resize_LL_image_dir = '../ThreeStageBinarization/image_patches/256X256_big_LL_image/'
+    LL_normalized_dir = '../ThreeStageBinarization/image_patches/256X256_LL_normalized/'
+    split_merge_DWT_0001(image_dir,DWT_4_image_dir,resize_LL_image_dir,LL_normalized_dir)
+
+def turn_img_to_gray(input_image_dir,input_rename_dir,input_gray_dir):
+    # os.makedirs(input_image_dir, exist_ok=True)
+    os.makedirs(input_rename_dir, exist_ok=True)
+    os.makedirs(input_gray_dir, exist_ok=True)
+    image_pathes = os.listdir(input_image_dir)
+    
+    i = 0
+    for image_path in image_pathes:
+        image_name = image_path.split('.')[0]
+        image_Filename_Extension = image_path.split('.')[1]
+        image = cv2.imread(os.path.join(input_image_dir,image_path))
+        cv2.imwrite("%s%s.%s"%(input_rename_dir,i,image_Filename_Extension), image)
+        i += 1
+    rename_pthes = os.listdir(input_rename_dir)
+    for rename_pth in rename_pthes:
+        image_rename = rename_pth.split('.')[0]
+        image_rename_Filename_Extension = rename_pth.split('.')[1]
+        rename_image = cv2.imread(os.path.join(input_rename_dir,rename_pth))
+        gray_image = cv2.cvtColor(rename_image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("%s%s.%s"%(input_gray_dir,image_rename,image_rename_Filename_Extension), gray_image)
+
+def image_split_3_colors(input_image_dir,input_split_3_colors):
+    os.makedirs(input_split_3_colors, exist_ok=True)
+    imagepthSSS = os.listdir(input_image_dir)
+    i = 0
+    for imagepth in imagepthSSS:
+        image_name = imagepth.split('.')[0]
+        image_filename_Extension = imagepth.split('.')[1]
+        rename_image = cv2.imread(os.path.join(input_image_dir,imagepth))
+        RED_COLOR_img, GREEN_COLOR_img, BLUE_COLOR_img = split_RGBThreeChannel(rename_image,i)
+        cv2.imwrite('%s%s_red.%s'%(input_split_3_colors,i,image_filename_Extension),RED_COLOR_img)
+        cv2.imwrite('%s%s_green.%s'%(input_split_3_colors,i,image_filename_Extension),GREEN_COLOR_img)
+        cv2.imwrite('%s%s_blue.%s'%(input_split_3_colors,i,image_filename_Extension),BLUE_COLOR_img)
+        i += 1
+
+def merge_RGBThreeChannel(R, G, B):
+    img = cv2.merge([B, G, R])
+    
+    return img
+def split_RGBThreeChannel(img,i):
+    (B, G, R) = cv2.split(img) # 3 channel
+    
+    # make all zeros channel
+    zeros = np.zeros(img.shape[:2], dtype = np.uint8)
+
+    print("R channel:%s image"%i)
+    RED_COLOR = merge_RGBThreeChannel(R=R, G=zeros, B=zeros)
+    
+    print("G channel:%s image"%i)
+    GREEN_COLOR = merge_RGBThreeChannel(R=zeros, G=G, B=zeros)
+    
+    print("B channel:%s image"%i)
+    BLUE_COLOR = merge_RGBThreeChannel(R=zeros, G=zeros, B=B)
+    
+    return RED_COLOR, GREEN_COLOR, BLUE_COLOR
+ 
+def split_merge_DWT(img,wavelet):
+    (B, G, R) = cv2.split(img)
+    coeffs_B = pywt.dwt2( B, wavelet )
+    coeffs_G = pywt.dwt2( G, wavelet )
+    coeffs_R = pywt.dwt2( R, wavelet )
+    LL_B, (LH_B, HL_B, HH_B) = coeffs_B
+    LL_G, (LH_G, HL_G, HH_G) = coeffs_G
+    LL_R, (LH_R, HL_R, HH_R) = coeffs_R
+    LL = cv2.merge([LL_B, LL_G, LL_R])
+    LH = cv2.merge([HL_B, HL_G, HL_R])
+    HL = cv2.merge([LH_B, LH_G, LH_R])
+    HH = cv2.merge([HH_B, HH_G, HH_R])
+    return LL,HL,LH,HH
+
+def DWT_big_LL( f, wavelet ):
+    
+	coeffs = pywt.dwt2( f, wavelet )
+	LL, (LH, HL, HH) = coeffs
+    	
+	ROW_1, COL_1 = LH.shape[:2]
+	nr1, nc1 = LL.shape[:2]
+    
+	big_LL_normalized = np.zeros( [nr1 * 2, nc1 * 2], dtype = 'uint8' )
+    # LL_normalized_image = np.zeros( [ROW_1])
+	# LL (Normalized for Display)
+	LL_normalized = np.zeros( [ROW_1, COL_1] )
+	cv2.normalize( LL, LL_normalized, 0, 255, cv2.NORM_MINMAX )
+	g = np.uint8( LL_normalized[:,:] )
+
+	big_LL_normalized[0:2*nr1,0:nc1*2] = cv2.resize(g,(nr1 * 2, nc1 * 2))
+
+	return big_LL_normalized
+
+    # nr2, nc2 = DWT_4_image.
+    # DWT_big_LL_image = np.zeros( [nr1])
+
+def DWT_image_db1( img ):
+	
+	coeffs = pywt.dwt2( img, 'db1' )
+	LL, (LH, HL, HH) = coeffs	
+	
+	nr1, nc1 = LL.shape[:2]
+	g = np.zeros( [nr1 * 2, nc1 * 2], dtype = 'uint8' )
+    
+    
+	g[0:nr1,0:nc1] = np.uint8(LL)
+	g[0:nr1,nc1:2*nc1] = np.uint8(LH) 
+	g[nr1:2*nr1,0:nc1] = np.uint8(HL)
+	g[nr1:2*nr1,nc1:nc1*2] = np.uint8(HH)	
+    
+	return g,g[0:nr1,0:nc1]
+
+def split_merge_DWT_0001(input_image_dir,input_DWT_4_image_dir,input_resize_LL,input_LL_normalized_dir):
+    os.makedirs(input_DWT_4_image_dir, exist_ok=True)
+    os.makedirs(input_resize_LL, exist_ok=True)
+    os.makedirs(input_LL_normalized_dir,exist_ok=True)
+    image_pathes = os.listdir(input_image_dir)
+    i = 0
+    for image_path in image_pathes:
+        image_Filename_Extension = image_path.split('.')[1]
+        image = cv2.imread(os.path.join(input_image_dir,image_path))
+        (B, G, R) = cv2.split(image)
+        i += 1
+        name_color = ["blue", "green" ,"red"]
+        for j in name_color:
+            for color in (B, G, R):
+                image_4 = DWT_image_db1(color)[0]
+                cv2.imwrite("%s%s_%s.%s"%(input_DWT_4_image_dir,i,j,image_Filename_Extension), image_4)
+                image_LL = DWT_image_db1(color)[1]
+                row_LL, column_LL = image_LL.shape[:2]
+                image_LL_re = cv2.resize(image_LL,(row_LL*2, column_LL*2))
+                cv2.imwrite("%s%s_%s.%s"%(input_resize_LL,i,j,image_Filename_Extension),image_LL_re)
+                LL_norm = DWT_image_GO(color)
+                cv2.imwrite("%s%s_%s(g1).%s"%(input_LL_normalized_dir,i,j,image_Filename_Extension),LL_norm)
+
+def DWT_big_LL_normalize( img ):
+    # img1 = cv2.imread( img)
+    coeffs = pywt.dwt2( img, 'db1' )
+    LL, (LH, HL, HH) = coeffs	
+	
+    nr1, nc1 = LL.shape[:2]
+    g = np.zeros( [nr1 * 2, nc1 * 2], dtype = 'uint8' )
+
+    # g[0:nr1,0:nc1] = np.uint8(LL)
+    # g[0:nr1,nc1:2*nc1] = np.uint8(LH) 
+    # g[nr1:2*nr1,0:nc1] = np.uint8(HL)
+    # g[nr1:2*nr1,nc1:nc1*2] = np.uint8(HH)	
+    nr1, nc1 = img.shape[:2]
+    g = np.zeros( [nr1 , nc1 ], dtype = 'uint8' )
+    LL_normalized = np.zeros( [nr1, nc1] )
+    cv2.normalize(LL,LL_normalized, 0, 255)
+    g = ( LL_normalized[:,:] )
+
+    return g  
+    
+def DWT_image_GO( f ):
+	nr, nc = f.shape[:2]
+	coeffs = pywt.dwt2( f,'db1' )
+	LL, (LH, HL, HH) = coeffs	
+	
+	nr1, nc1 = LL.shape[:2]
+	g = np.zeros( [nr1 * 2, nc1 * 2], dtype = 'uint8' )
+
+	# LL (Normalized for Display)
+	LL_normalized = np.zeros( [nr1, nc1] )
+	cv2.normalize( LL, LL_normalized, 0, 255, cv2.NORM_MINMAX )
+	g = np.uint8( LL_normalized[:,:] )
+	g321 = cv2.resize(g,(nr1*2, nc1*2))
+	
+	return g321
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,111 @@
+## [Three-stage binarization of color document images based on discrete wavelet transform and generative adversarial networks](https://arxiv.org/)
+### Stage-1 Flowchart
+<p align="center">
+  <img src="Figure/figure_stage_1.jpg" width="480" title="Stage-1">
+</p>
+
+### Stage-2 Flowchart
+<p align="center">
+  <img src="Figure/figure_stage_2.jpg" width="480" title="Stage-2">
+</p>
+
+### Stage-3 Flowchart
+<p align="center">
+  <img src="Figure/figure_stage_3.jpg" width="480" title="Stage-3">
+</p>
+
+## Abstract
+The efficient segmentation of foreground text information from the background in degraded color document images is a topic of concern. Due to the imperfect preservation of ancient documents over a long period of time, various types of degradation, including staining, yellowing, and ink seepage, have seriously affected the results of image binarization. In this paper, a three-stage method is proposed for image enhancement and binarization of degraded color document images by using discrete wavelet transform (DWT) and generative adversarial network (GAN). In Stage-1, we use DWT and retain the LL subband images to achieve the image enhancement. In Stage-2, the original input image is split into four (Red, Green, Blue and Gray) single-channel images, each of which trains the independent adversarial networks. The trained adversarial network models are used to extract the color foreground information from the images. In Stage-3, in order to combine global and local features, the output image from Stage-2 and the original input image are used to train the independent adversarial networks for document binarization. The experimental results demonstrate that our proposed method outperforms many classical and state-of-the-art (SOTA) methods on the Document Image Binarization Contest (DIBCO) dataset.
+
+## Requirements
+* Linux (Ubuntu)
+* Python >= 3.6 (Pytorch)
+* NVIDIA GPU + CUDA CuDNN
+
+## Installation
+* Install [segmentation_models](https://github.com/qubvel/segmentation_models.pytorch)
+```
+    pip install segmentation-models-pytorch
+```
+* Install [pytesseract](https://github.com/madmaze/pytesseract)
+```
+    pip install pytesseract
+```
+* Download [tesseract data](https://github.com/tesseract-ocr/tessdata_best)
+```
+    conda env create -f environment.yml
+```
+
+## Dataset
+* Train: 
+  
+  DIBCO 2009, H-DIBCO 2010, H-DIBCO 2012, PHIBD, SMADI, Bickley Diary Dataset
+  [(Download Link)](https://www.dropbox.com/s/3qwv3jntmgu4rf9/Trainset.zip?dl=0)
+  
+* Test: 
+
+  DIBCO 2011, DIBCO 2013, H-DIBCO 2014, H-DIBCO 2016, DIBCO 2017, H-DIBCO2018
+  [(Download Link)](https://www.dropbox.com/s/54ye0mtdcvqas4o/Testset.zip?dl=0)
+
+## Usage
+* Patch per datasets
+
+    Example: 512×512
+<p align="center">
+<img src="img_512X512/PHIBD_2012_4_h0.png" width="120" title="512_1">
+<img src="img_512X512/PHIBD_2012_4_r0.png" width="120" title="512_2">
+<img src="img_512X512/PHIBD_2012_4_r1.png" width="120" title="512_3">
+<img src="img_512X512/PHIBD_2012_4_r2.png" width="120" title="512_4">
+<img src="img_512X512/PHIBD_2012_4_r3.png" width="120" title="512_5">
+<img src="img_512X512/PHIBD_2012_4_v0.png" width="120" title="512_6">
+</p>
+
+```
+    python3 image_to_256.py
+    python3 image_to_512.py
+```
+
+* Discrete Wavelet Transform
+
+    Example: Blue, Green, Red
+<p align="center">
+<img src="img_dwt/blue.bmp" width="240" title="blue">
+<img src="img_dwt/green.bmp" width="240" title="green">
+<img src="img_dwt/red.bmp" width="240" title="red">
+</p>
+
+```
+    python3 image_dwt_original.py
+    python3 image_dwt_256.py
+```
+* Train the model
+```
+    python3 train_stage2.py
+    python3 predict_for_stage3.py
+    python3 train_stage3.py
+    python3 train_stage3_resize.py
+```
+
+* Evaluation the model
+```
+    python3 eval_stage3_all.py
+```
+
+## Comparison Experiment of Stage-1 on the H-DIBCO 2014
+| Option | Input | GT | FM↑ | p-FM↑ | PSNR↑ | DRD↓ |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: |
+| 1 | \ | \ | 96.50 | 97.50 | 22.08 | 1.01 |
+| 2 | \ | DWT(LL) | 96.52 | 97.70 | 22.15 | 0.99 |
+| 3 | \ | DWT(LL)+Norm | **96.88** | **98.03** | **22.68** | **0.89** |
+| 4 | DWT(LL) | \  | 95.94 | 96.91 | 21.31 | 1.19 |
+| 5 | DWT(LL)+Norm | \ | 96.29 | 97.48 | 22.05 | 1.16 |
+| 6 | DWT(LL) | DWT(LL) | 96.60 | 97.60 | 22.27 | 0.97 |
+| 7 | DWT(LL)+Norm | DWT(LL)+Norm | 96.77 | 97.89 | 22.52 | 0.91 | 
+
+* Option 2 is `method1_NA_LL.py`
+* Option 3 is `method2_NA_LLandNorm.py`
+* Option 6 is `method3_LL_LL.py`
+* Option 7 is `method4_LLandNorm_LLandNorm`
+
+## References
+[DocumentBinarization](https://github.com/opensuh/DocumentBinarization)
