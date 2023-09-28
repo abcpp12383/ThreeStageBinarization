ada354c076c98811768cdbc279d95e8897e556d7
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-09-28 17:03:14 +0800

    Update eval_stage3_all.py

--- DIBCO/eval_stage3_all.py
+++ DIBCO/eval_stage3_all.py
@@ -13,10 +13,10 @@ from Base.metrics import get_metric
 parser = argparse.ArgumentParser()
 parser.add_argument("--gpu", type=str, default='0', help="GPU number")
 parser.add_argument('--lambda_bce', type=float, default=50.0, help='bce weight')
-parser.add_argument('--base_model_name', type=str, default='efficientnet-b5', help='base_model_name')
+parser.add_argument('--base_model_name', type=str, default='efficientnet-b6', help='base_model_name')
 parser.add_argument('--encoder_weights', type=str, default='imagenet', help='encoder_weights')
-parser.add_argument('--generator_lr', type=float, default=1e-4, help='generator learning rate')
-parser.add_argument('--discriminator_lr', type=float, default=1e-4, help='discriminator learning rate')
+parser.add_argument('--generator_lr', type=float, default=2e-4, help='generator learning rate')
+parser.add_argument('--discriminator_lr', type=float, default=2e-4, help='discriminator learning rate')
 parser.add_argument('--batch_size', type=int, default=16, help='batch size')
 parser.add_argument('--threshold', type=float, default=0.30, help='threshold for bgr mask')
 
